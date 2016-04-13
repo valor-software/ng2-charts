cd236fb6f87c8d46f0ef4f8b4cfcf8c96d8d78c5
Dmitriy Shekhovtsov <valorkin@gmail.com>
2016-04-13 16:41:29 +0300

    chore(flow): update gulp tasks

--- gulp-tasks/lint.js
+++ gulp-tasks/lint.js
@@ -1,23 +1,20 @@
-var gulp = require('gulp');
-var esLint = require('gulp-eslint');
-var tslint = require('gulp-tslint');
 
-var paths = gulp.paths;
+'use strict';
 
-gulp.task('eslint', function() {
-  return gulp.src(paths.jssrc)
-    .pipe(esLint({useEslintrc: true}))
-    .pipe(esLint.format())
-    .pipe(esLint.failOnError());
-});
+const gulp = require('gulp');
+const tslint = require('gulp-tslint');
+const paths = gulp.paths;
+const tslintConf = require('../tslint.json');
 
-gulp.task('tslint', function() {
-  return gulp.src(paths.tssrc)
-    .pipe(tslint())
-    .pipe(tslint.report('verbose', {
+gulp.task('tslint', () =>
+  gulp
+    .src(paths.tssrc)
+    .pipe(tslint(tslintConf))
+    .pipe(tslint.report('prose', {
       emitError: true,
-      reportLimit: 0
-    }));
-});
+      summarizeFailureOutput: true,
+      reportLimit: 50
+    }))
+);
 
-gulp.task('lint', ['tslint', 'eslint']);
+gulp.task('lint', ['tslint']);
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,25 @@
-var gulp = require('gulp');
+'use strict';
+
+const gulp = require('gulp');
 
 gulp.paths = {
   tssrc: [
     '**/*.ts',
+    '!**/*.d.ts',
     '!node_modules/**/*',
-    '!dist/**/*',
-    '!typings/**/*',
-    '!**/*.{ts,coffee}.js'],
+    '!bundles/**/*',
+    '!typings/**/*'],
   jssrc: [
     '*.js',
-    '!angular2-bootstrap.js',
     'gulp-tasks/*.js',
-    '!node_modules',
+    '!bundles/*.js',
+    '!ng2-bootstrap.js',
+    '!node_modules/**/*',
     '!**/*.{ts,coffee}.js']
 };
 
 require('require-dir')('./gulp-tasks');
 
-var clean = require('gulp-clean');
-gulp.task('clean', function () {
-  return gulp.src('dist', {read: false})
-    .pipe(clean());
-});
-
-gulp.task('default', function () {
+gulp.task('default', () => {
   gulp.start('lint');
 });
