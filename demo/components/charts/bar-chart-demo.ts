ec1008f025ff87b3ec525e576a61cf98db509df9
Oleksandr Telnov <oleksandr.telnov@valor-software.com>
2016-08-04 16:06:00 +0300

    update dependencies to latest version

--- demo/components/charts/bar-chart-demo.ts
+++ demo/components/charts/bar-chart-demo.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
-import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass} from '@angular/common';
+import {CORE_DIRECTIVES, NgClass} from '@angular/common';
+import {FORM_DIRECTIVES} from '@angular/forms';
 
 import {CHART_DIRECTIVES} from '../../../ng2-charts';
 
