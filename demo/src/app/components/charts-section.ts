cba21b0c2f46ba573980f50190ca26bf7be9d926
artem <artem.frolov@valor-softvare.com>
2017-05-30 16:10:26 +0300

    fix (demo): fix links, navigation

--- demo/src/app/components/charts-section.ts
+++ demo/src/app/components/charts-section.ts
@@ -6,38 +6,78 @@ let doc = require('html-loader!markdown-loader!../../doc.md');
 @Component({
   selector: 'charts-section',
   template: `
-<br>
-<div class="row">
-  <h2>API</h2>
-  <div class="card card-block panel panel-default panel-body" [innerHTML]="doc"></div>
-</div>
+    <br>
+    <div class="row">
+      <h2>API</h2>
+      <div class="card card-block panel panel-default panel-body" [innerHTML]="doc"></div>
+    </div>
 
-<section [attr.id]="name">
-  <div class="row"><h1>{{name}}<small>(<a [attr.href]="src">src</a>)</small></h1></div>
+    <section [attr.id]="name">
+      <div class="row">
+        <h1>{{name}}
+          <small>(<a [attr.href]="src">src</a>)</small>
+        </h1>
+      </div>
 
-  <hr>
+      <hr>
 
-  <div class="row">
-    <h2>Example</h2>
-      
-    <chart-section [ts]="desc.lineChart.ts" [html]="desc.lineChart.html" [heading]="desc.lineChart.heading"><line-chart-demo></line-chart-demo></chart-section>
-    <chart-section [ts]="desc.barChart.ts" [html]="desc.barChart.html" [heading]="desc.barChart.heading"><bar-chart-demo></bar-chart-demo></chart-section>
-    <chart-section [ts]="desc.douChart.ts" [html]="desc.douChart.html" [heading]="desc.douChart.heading"><doughnut-chart-demo></doughnut-chart-demo></chart-section>
-    <chart-section [ts]="desc.radarChart.ts" [html]="desc.radarChart.html" [heading]="desc.radarChart.heading"><radar-chart-demo></radar-chart-demo></chart-section>
-    <chart-section [ts]="desc.pieChart.ts" [html]="desc.pieChart.html" [heading]="desc.pieChart.heading"><pie-chart-demo></pie-chart-demo></chart-section>
-    <chart-section [ts]="desc.polarChart.ts" [html]="desc.polarChart.html" [heading]="desc.polarChart.heading"><polar-area-chart-demo></polar-area-chart-demo></chart-section>
-    <chart-section [ts]="desc.baseChart.ts" [html]="desc.baseChart.html" [heading]="desc.baseChart.heading"><base-chart-demo></base-chart-demo></chart-section>
-  </div>
+      <div class="row">
+        <h2>Example</h2>
 
-  <br>
+        <chart-section [ts]="desc.lineChart.ts"
+                       [html]="desc.lineChart.html"
+                      
+                       [id]="desc.lineChart.id"
+                       [heading]="desc.lineChart.heading">
+          <line-chart-demo></line-chart-demo>
+        </chart-section>
+        <chart-section [ts]="desc.barChart.ts"
+                       [html]="desc.barChart.html"
+                       [id]="desc.barChart.id"
+                       [heading]="desc.barChart.heading">
+          <bar-chart-demo></bar-chart-demo>
+        </chart-section>
+        <chart-section [ts]="desc.douChart.ts"
+                       [html]="desc.douChart.html"
+                       [id]="desc.douChart.id"
+                       [heading]="desc.douChart.heading">
+          <doughnut-chart-demo></doughnut-chart-demo>
+        </chart-section>
+        <chart-section [ts]="desc.radarChart.ts"
+                       [html]="desc.radarChart.html"
+                       [id]="desc.radarChart.id"
+                       [heading]="desc.radarChart.heading">
+          <radar-chart-demo></radar-chart-demo>
+        </chart-section>
+        <chart-section [ts]="desc.pieChart.ts"
+                       [html]="desc.pieChart.html"
+                       [id]="desc.pieChart.id"
+                       [heading]="desc.pieChart.heading">
+          <pie-chart-demo></pie-chart-demo>
+        </chart-section>
+        <chart-section [ts]="desc.polarChart.ts"
+                       [html]="desc.polarChart.html"
+                       [id]="desc.polarChart.id"
+                       [heading]="desc.polarChart.heading">
+          <polar-area-chart-demo></polar-area-chart-demo>
+        </chart-section>
+        <chart-section [ts]="desc.baseChart.ts"
+                       [html]="desc.baseChart.html"
+                       [id]="desc.baseChart.id"
+                       [heading]="desc.baseChart.heading">
+          <base-chart-demo></base-chart-demo>
+        </chart-section>
+      </div>
 
-</section>
+      <br>
+
+    </section>
   `
 })
 
 export class ChartsSectionComponent {
   public name: string = 'Charts';
-  public src: string = 'https://github.com/valor-software/ng2-charts/blob/master/components/charts/charts.ts';
+  public src: string = 'https://github.com/valor-software/ng2-charts/blob/development/src/charts/charts.ts';
   public doc: string = doc;
   public desc: any = {
     lineChart: {
