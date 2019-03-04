4ce53e56496954d483383490493a1d1138706f70
Aviad Pineles <paviad2@gmail.com>
2019-03-04 11:54:12 +0200

    fix: Fixed code which didn't run when using AOT

--- src/app/chart-host/chart-host.component.ts
+++ src/app/chart-host/chart-host.component.ts
@@ -1,4 +1,43 @@
-import { Component, OnInit, ContentChild, AfterContentInit } from '@angular/core';
+import { Component, OnInit, ContentChild, AfterContentInit, Input } from '@angular/core';
+
+export const chartTypes = {
+  bar: {
+    heading: 'Bar Chart',
+    ts: require('!!raw-loader!../bar-chart/bar-chart.component.ts'),
+    html: require('!!raw-loader!../bar-chart/bar-chart.component.html'),
+  },
+  doughnut: {
+    heading: 'Doughnut Chart',
+    ts: require('!!raw-loader!../doughnut-chart/doughnut-chart.component.ts'),
+    html: require('!!raw-loader!../doughnut-chart/doughnut-chart.component.html'),
+  },
+  dynamic: {
+    heading: 'Dynamic Chart',
+    ts: require('!!raw-loader!../dynamic-chart/dynamic-chart.component.ts'),
+    html: require('!!raw-loader!../dynamic-chart/dynamic-chart.component.html'),
+  },
+  line: {
+    heading: 'Line Chart',
+    ts: require('!!raw-loader!../line-chart/line-chart.component.ts'),
+    html: require('!!raw-loader!../line-chart/line-chart.component.html'),
+  },
+  pie: {
+    heading: 'Pie Chart',
+    ts: require('!!raw-loader!../pie-chart/pie-chart.component.ts'),
+    html: require('!!raw-loader!../pie-chart/pie-chart.component.html'),
+  },
+  'polar-area': {
+    heading: 'Polar Area Chart',
+    ts: require('!!raw-loader!../polar-area-chart/polar-area-chart.component.ts'),
+    html: require('!!raw-loader!../polar-area-chart/polar-area-chart.component.html'),
+  },
+  radar: {
+    heading: 'Radar Chart',
+    ts: require('!!raw-loader!../radar-chart/radar-chart.component.ts'),
+    html: require('!!raw-loader!../radar-chart/radar-chart.component.html'),
+  },
+};
+
 
 @Component({
   selector: 'app-chart-host',
@@ -6,6 +45,7 @@ import { Component, OnInit, ContentChild, AfterContentInit } from '@angular/core
   styleUrls: ['./chart-host.component.scss']
 })
 export class ChartHostComponent implements OnInit, AfterContentInit {
+  @Input() chartType: string;
   @ContentChild('main') content;
 
   html: string;
@@ -14,50 +54,14 @@ export class ChartHostComponent implements OnInit, AfterContentInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   ngAfterContentInit(): void {
-    if (!this.content) {
-      return;
-    }
-    const compName = this.content.__proto__.constructor.name;
-    switch (compName) {
-      case 'BarChartComponent':
-        this.heading = 'Bar Chart';
-        this.ts = require('!!raw-loader!../bar-chart/bar-chart.component.ts');
-        this.html = require('!!raw-loader!../bar-chart/bar-chart.component.html');
-        break;
-      case 'DoughnutChartComponent':
-        this.heading = 'Doughnut Chart';
-        this.ts = require('!!raw-loader!../doughnut-chart/doughnut-chart.component.ts');
-        this.html = require('!!raw-loader!../doughnut-chart/doughnut-chart.component.html');
-        break;
-      case 'DynamicChartComponent':
-        this.heading = 'Dynamic Chart';
-        this.ts = require('!!raw-loader!../dynamic-chart/dynamic-chart.component.ts');
-        this.html = require('!!raw-loader!../dynamic-chart/dynamic-chart.component.html');
-        break;
-      case 'LineChartComponent':
-        this.heading = 'Line Chart';
-        this.ts = require('!!raw-loader!../line-chart/line-chart.component.ts');
-        this.html = require('!!raw-loader!../line-chart/line-chart.component.html');
-        break;
-      case 'PieChartComponent':
-        this.heading = 'Pie Chart';
-        this.ts = require('!!raw-loader!../pie-chart/pie-chart.component.ts');
-        this.html = require('!!raw-loader!../pie-chart/pie-chart.component.html');
-        break;
-      case 'PolarAreaChartComponent':
-        this.heading = 'Polar Area Chart';
-        this.ts = require('!!raw-loader!../polar-area-chart/polar-area-chart.component.ts');
-        this.html = require('!!raw-loader!../polar-area-chart/polar-area-chart.component.html');
-        break;
-      case 'RadarChartComponent':
-        this.heading = 'Radar Chart';
-        this.ts = require('!!raw-loader!../radar-chart/radar-chart.component.ts');
-        this.html = require('!!raw-loader!../radar-chart/radar-chart.component.html');
-        break;
+    const compName = this.chartType;
+    if (chartTypes[compName]) {
+      this.heading = chartTypes[compName].heading;
+      this.html = chartTypes[compName].html;
+      this.ts = chartTypes[compName].ts;
     }
   }
 }
