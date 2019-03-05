eb52372ab97799b469ecfb883374774cc7120f09
Aviad Pineles <paviad2@gmail.com>
2019-03-05 23:57:57 +0200

    demo: Show dynamic change in legend or labels
    Added buttons to pie chart to allow toggling of
    legend visibility, and to randomize the labels
    dynamically.

--- src/app/pie-chart/pie-chart.component.ts
+++ src/app/pie-chart/pie-chart.component.ts
@@ -12,6 +12,7 @@ export class PieChartComponent implements OnInit {
   public pieChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
   public pieChartData: SingleDataSet = [300, 500, 100];
   public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
 
   constructor() { }
 
@@ -25,5 +26,16 @@ export class PieChartComponent implements OnInit {
 
   public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
     console.log(event, active);
+  }
+
+  changeLabels() {
+    const words = ['hen', 'variable', 'embryo', 'instal', 'pleasant', 'physical', 'bomber', 'army', 'add', 'film',
+      'conductor', 'comfortable', 'flourish', 'establish', 'circumstance', 'chimney', 'crack', 'hall', 'energy',
+      'treat', 'window', 'shareholder', 'division', 'disk', 'temptation', 'chord', 'left', 'hospital', 'beef',
+      'patrol', 'satisfied', 'academy', 'acceptance', 'ivory', 'aquarium', 'building', 'store', 'replace', 'language',
+      'redeem', 'honest', 'intention', 'silk', 'opera', 'sleep', 'innocent', 'ignore', 'suite', 'applaud', 'funny'];
+    const randomWord = () => words[Math.trunc(Math.random() * words.length)];
+    this.pieChartLabels = Array.apply(null, { length: 3 }).map(_ => randomWord());
+    console.log('kabels', this.pieChartLabels, randomWord());
   }
 }
