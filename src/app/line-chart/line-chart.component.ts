175cb0c4fccc9601d21c2107899a3afc68fb4567
Aviad Pineles <paviad2@gmail.com>
2019-03-06 21:57:07 +0200

    demo: Add 2nd axis to line chart

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -11,11 +11,23 @@ export class LineChartComponent implements OnInit {
   public lineChartData: ChartDataSets[] = [
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
     { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    { data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C' }
+    { data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C', yAxisID: 'y-axis-1' }
   ];
   public lineChartLabels: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions: ChartOptions = {
-    responsive: true
+    responsive: true,
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-0',
+          position: 'left',
+        },
+        {
+          id: 'y-axis-1',
+          position: 'right',
+        }
+      ]
+    }
   };
   public lineChartColors: Color[] = [
     { // grey
