d4d2cce17af94795d618a01630571588a6430992
Aviad Pineles <paviad2@gmail.com>
2019-03-06 21:09:07 +0200

    demo: Added dark/light theme support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.scss' ]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor() { }
+  private pSelectedTheme = 'lala';
+  public get selectedTheme() {
+    return this.pSelectedTheme;
+  }
+  public set selectedTheme(value) {
+    this.renderer.removeClass(this.document.body, this.pSelectedTheme);
+    this.pSelectedTheme = value;
+    this.renderer.addClass(this.document.body, value);
+  }
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2) {
+    this.selectedTheme = 'ng2-charts-demo-light-theme';
+  }
 
   ngOnInit() { }
 }
