c99ce4dd2d03e6ec650a97c0fb71b3ca7a87b9b4
ludmilanesvitiy <ludmila.nesvitiy@careerbuilder.com>
2019-03-11 19:27:30 +0200

    fix(common): small enhancements to charts, buttons after testing

--- src/app/dynamic-chart/dynamic-chart.component.ts
+++ src/app/dynamic-chart/dynamic-chart.component.ts
@@ -37,23 +37,6 @@ export class DynamicChartComponent implements OnInit {
   }
 
   public randomize(): void {
-    // Only Change 3 values
-    const data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      (Math.random() * 100),
-      56,
-      (Math.random() * 100),
-      40];
-    const clone = JSON.parse(JSON.stringify(this.barChartData));
-    clone[0].data = data;
-    this.barChartData = clone;
-    /**
-     * (My guess), for Angular to recognize the change in the dataset
-     * it has to change the dataset variable directly,
-     * so one way around it, is to clone the data, change it and then
-     * assign it;
-     */
+    this.barChartType = this.barChartType === 'bar' ? 'line' : 'bar';
   }
 }
