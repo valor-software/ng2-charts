79e8b5b4e4b28458f58088710abb70af4b24d627
ludmilanesvitiy <ludmila.nesvitiy@careerbuilder.com>
2019-03-15 14:05:28 +0200

    test(common): add main and chart test with applitools and cypress

---
+++ cypress/integration/screenshot-test.js
@@ -0,0 +1,32 @@
+var Eyes = require('eyes.images').Eyes;
+const fs = require('fs');
+const urls = require('../fixtures/example').componentsUrls;
+
+var eyes = new Eyes();
+
+eyes.setApiKey('Nls100t102uNgLhLpblVUdA3tP104MQQS9VUYgHQ5tYOUuIxU110');
+
+eyes.setHostingApp("Chrome");
+
+eyes.setHostOS('MacOS latest');
+
+
+async function forEachAsync(array, callback) {
+  let i = 0;
+  for (; i < array.length; i++) {
+    await callback(array[i], i, array);
+  }
+}
+
+const testResults = eyes.open("NG2-charts", "Charts comparison", {width: 1366, height: 768}).then(async function () {
+  return forEachAsync(urls, async (url) => {
+    const image = fs.readFileSync(`./cypress/screenshots/main-test.js/Charts -- ${url}.png`);
+    await eyes.checkImage(image, url);
+  });
+}).then(function () {
+  return eyes.close();
+});
+
+testResults.then(function (results) {
+  console.log("Results: ", results);
+});
