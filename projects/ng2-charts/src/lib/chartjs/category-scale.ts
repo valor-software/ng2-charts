0feb965257425d811457fdfe34bc459b355aab17
Aviad Pineles <paviad2@gmail.com>
2019-03-21 15:55:13 +0200

    feat: Added financial chart

--- projects/ng2-charts/src/lib/chartjs/category-scale.ts
+++ projects/ng2-charts/src/lib/chartjs/category-scale.ts
@@ -1,8 +1,9 @@
 import { CategoryTickOptions } from './category-tick-options';
 import { CartesianScale } from './cartesian-scale';
+import { BaseChartMetaConfig } from './base-chart-meta-config';
 
 // [Axes/Cartesian/Category](https://www.chartjs.org/docs/latest/axes/cartesian/category.html)
-export interface CategoryScale extends CartesianScale {
-  type?: 'category';
+export interface CategoryScale<T extends BaseChartMetaConfig> extends CartesianScale<T> {
+  type?: 'category' | string;
   ticks?: CategoryTickOptions;
 }
