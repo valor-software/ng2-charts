08bc45a5118a6aa810e732aa75136150cf865d0f
Aviad Pineles <paviad2@gmail.com>
2019-03-21 21:02:57 +0200

    fix: Fix TimeScale classes

--- projects/ng2-charts/src/lib/chartjs/scale-union.ts
+++ projects/ng2-charts/src/lib/chartjs/scale-union.ts
@@ -2,13 +2,13 @@ import { RadialLinearScale } from './radial-linear-scale';
 import { LogarithmicScale } from './logarithmic-scale';
 import { LinearScale } from './linear-scale';
 import { CategoryScale } from './category-scale';
-import { TimeOptions } from './time-options';
 import { ChartMetaConfig } from './chart-meta-config';
+import { TimeScale } from './time-scale';
 
 export type ScaleUnion =
   | CategoryScale<ChartMetaConfig>
   | LinearScale<ChartMetaConfig>
   | LogarithmicScale<ChartMetaConfig>
-  | TimeOptions<ChartMetaConfig>
+  | TimeScale<ChartMetaConfig>
   | RadialLinearScale<ChartMetaConfig>
   ;
