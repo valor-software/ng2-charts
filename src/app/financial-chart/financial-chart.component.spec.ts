06c5dad5c322ff1d77152871307e8af65558a3a8
Aviad Pineles <paviad2@gmail.com>
2019-03-29 13:12:09 +0300

    test: Fix financial chart tests

--- src/app/financial-chart/financial-chart.component.spec.ts
+++ src/app/financial-chart/financial-chart.component.spec.ts
@@ -1,7 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { MarkdownModule } from 'ngx-markdown';
+import { HighlightModule } from 'ngx-highlightjs';
+import typescript from 'highlight.js/lib/languages/typescript';
+import xml from 'highlight.js/lib/languages/xml';
 
 import { FinancialChartComponent } from './financial-chart.component';
 import { ChartsModule } from 'ng2-charts';
+
+export function hljsLanguages() {
+  return [
+    { name: 'typescript', func: typescript },
+    // { name: 'html', func: html },
+    // {name: 'scss', func: scss},
+    {name: 'xml', func: xml}
+  ];
+}
 
 describe('FinancialChartComponent', () => {
   let component: FinancialChartComponent;
@@ -11,7 +26,12 @@ describe('FinancialChartComponent', () => {
     TestBed.configureTestingModule({
       declarations: [FinancialChartComponent],
       imports: [
+        HttpClientModule,
         ChartsModule,
+        MarkdownModule.forRoot({ loader: HttpClient }),
+        HighlightModule.forRoot({
+          languages: hljsLanguages,
+        }),
       ],
     })
       .compileComponents();
