da3ef426d6ea62c12a45465a5c3bcb2a096fc655
Aviad Pineles <paviad2@gmail.com>
2019-03-31 14:54:03 +0300

    demo: Show change detection in legend position

--- src/app/pie-chart/pie-chart.component.ts
+++ src/app/pie-chart/pie-chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartType, ChartOptions } from 'chart.js';
-import { SingleDataSet, Label } from 'ng2-charts';
+import { Label } from 'ng2-charts';
 import * as pluginDataLabels from 'chartjs-plugin-datalabels';
 
 @Component({
@@ -12,6 +12,9 @@ export class PieChartComponent implements OnInit {
   // Pie
   public pieChartOptions: ChartOptions = {
     responsive: true,
+    legend: {
+      position: 'top',
+    },
     plugins: {
       datalabels: {
         formatter: (value, ctx) => {
@@ -22,10 +25,15 @@ export class PieChartComponent implements OnInit {
     }
   };
   public pieChartLabels: Label[] = [['Download', 'Sales'], ['In', 'Store', 'Sales'], 'Mail Sales'];
-  public pieChartData: SingleDataSet = [300, 500, 100];
+  public pieChartData: number[] = [300, 500, 100];
   public pieChartType: ChartType = 'pie';
   public pieChartLegend = true;
   public pieChartPlugins = [pluginDataLabels];
+  public pieChartColors = [
+    {
+      backgroundColor: ['rgba(255,0,0,0.3)', 'rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)'],
+    },
+  ];
 
   constructor() { }
 
@@ -49,5 +57,21 @@ export class PieChartComponent implements OnInit {
       'redeem', 'honest', 'intention', 'silk', 'opera', 'sleep', 'innocent', 'ignore', 'suite', 'applaud', 'funny'];
     const randomWord = () => words[Math.trunc(Math.random() * words.length)];
     this.pieChartLabels = Array.apply(null, { length: 3 }).map(_ => randomWord());
+  }
+
+  addSlice() {
+    this.pieChartLabels.push(['Line 1', 'Line 2', 'Line 3']);
+    this.pieChartData.push(400);
+    this.pieChartColors[0].backgroundColor.push('rgba(196,79,244,0.3)');
+  }
+
+  removeSlice() {
+    this.pieChartLabels.pop();
+    this.pieChartData.pop();
+    this.pieChartColors[0].backgroundColor.pop();
+  }
+
+  changeLegendPosition() {
+    this.pieChartOptions.legend.position = this.pieChartOptions.legend.position === 'left' ? 'top' : 'left';
   }
 }
