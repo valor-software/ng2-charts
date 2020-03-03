efc6b95671bf8181a26d8090a88361e98d7f7d87
Joachim Damsgaard <nrone.dk@gmail.com>
2020-03-03 14:13:29 +0100

    Adding typescript support for CanvasGradient
    Adding CanvasGradient type for background and border color. See https://codepen.io/plavookac/pen/xgYYNV

--- projects/ng2-charts/src/lib/color.ts
+++ projects/ng2-charts/src/lib/color.ts
@@ -1,27 +1,27 @@
 // private helper functions
 export interface Color {
-  backgroundColor?: string | string[];
+  backgroundColor?: string | string[] | CanvasGradient;
   borderWidth?: number | number[];
-  borderColor?: string | string[];
+  borderColor?: string | string[] | CanvasGradient;
   borderCapStyle?: string;
   borderDash?: number[];
   borderDashOffset?: number;
   borderJoinStyle?: string;
 
-  pointBorderColor?: string | string[];
-  pointBackgroundColor?: string | string[];
+  pointBorderColor?: string | string[] | CanvasGradient;
+  pointBackgroundColor?: string | string[] | CanvasGradient;
   pointBorderWidth?: number | number[];
 
   pointRadius?: number | number[];
   pointHoverRadius?: number | number[];
   pointHitRadius?: number | number[];
 
-  pointHoverBackgroundColor?: string | string[];
-  pointHoverBorderColor?: string | string[];
+  pointHoverBackgroundColor?: string | string[] | CanvasGradient;
+  pointHoverBorderColor?: string | string[] | CanvasGradient;
   pointHoverBorderWidth?: number | number[];
   pointStyle?: string | string[];
 
-  hoverBackgroundColor?: string | string[];
-  hoverBorderColor?: string | string[];
+  hoverBackgroundColor?: string | string[] | CanvasGradient;
+  hoverBorderColor?: string | string[] | CanvasGradient;
   hoverBorderWidth?: number;
 }
