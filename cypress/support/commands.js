d84e3a81685f30b6c4f0a8dbf7e71c264c7ef504
Marco Santarelli <marco@backbase.com>
2020-09-13 14:05:44 +0200

    Use ssim for image comparison

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,8 @@
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
-addMatchImageSnapshotCommand();
+addMatchImageSnapshotCommand({
+  failureThreshold: 0.01, // threshold for entire image
+  failureThresholdType: 'percent', // percent of image or number of pixels
+  customDiffConfig: { threshold: 0.1 }, // threshold for pixelmatch
+  comparisonMethod: 'ssim'
+});
