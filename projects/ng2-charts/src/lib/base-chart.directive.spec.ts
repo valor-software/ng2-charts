d7183af62192bb9fdd007b5136d2a8900c2c6bae
Marco Santarelli <marco@backbase.com>
2020-10-08 18:09:46 +0200

    Updated dependencies Fixed lib tests Temporarily skip check on ts libraries

--- projects/ng2-charts/src/lib/base-chart.directive.spec.ts
+++ projects/ng2-charts/src/lib/base-chart.directive.spec.ts
@@ -1,9 +1,26 @@
 import { BaseChartDirective } from './base-chart.directive';
-import { ElementRef } from '@angular/core';
+import { By } from "@angular/platform-browser";
+import { TestBed } from "@angular/core/testing";
+import { Component } from "@angular/core";
+
+@Component({
+  template: '<canvas baseChart></canvas>'
+})
+class TestComponent {}
 
 describe('BaseChartDirective', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TestComponent,
+        BaseChartDirective
+      ]
+    });
+  });
+
   it('should create an instance', () => {
-    const directive = new BaseChartDirective(null as ElementRef);
+    const fixture = TestBed.createComponent(TestComponent);
+    const directive = fixture.debugElement.query(By.directive(BaseChartDirective));
     expect(directive).toBeTruthy();
   });
 });
