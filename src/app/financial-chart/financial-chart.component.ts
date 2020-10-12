cbea3903758be9f49d8da8cf39ecf8b5ca142d06
Marco Santarelli <marco@backbase.com>
2020-10-12 15:40:51 +0200

    Fixed financial-chart

--- src/app/financial-chart/financial-chart.component.ts
+++ src/app/financial-chart/financial-chart.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import 'dist/chartjs-chart-financial/chartjs-chart-financial';
-// import 'chartjs-adapter-luxon';
+import 'chartjs-adapter-date-fns';
 import { BaseChartDirective } from 'ng2-charts';
-import { DateTime } from 'luxon';
 import { IChartOptions } from 'chart.js';
+import { enUS } from 'date-fns/locale';
+import { add, parseISO } from "date-fns";
+
 
 @Component({
   selector: 'app-financial-chart',
@@ -12,18 +14,33 @@ import { IChartOptions } from 'chart.js';
 })
 export class FinancialChartComponent implements OnInit {
   barCount = 60;
-  initialDateStr = '01 Apr 2017 00:00 Z';
+  initialDateStr = '2017-04-01T00:00:00';
 
-  public financialChartData = [
-    {
+  public financialChartData = {
+    datasets: [ {
       label: 'CHRT - Chart.js Corporation',
       data: this.getRandomData(this.initialDateStr, this.barCount),
       barThickness: 10
-    }
-  ];
+    } ]
+  };
   public financialChartOptions: IChartOptions = {
     responsive: true,
     maintainAspectRatio: false,
+    scales: {
+      x: {
+        time: {
+          unit: 'day'
+        },
+        adapters: {
+          date: {
+            locale: enUS
+          }
+        },
+        ticks: {
+          source: 'auto'
+        }
+      }
+    }
   };
   public financialChartColors = [
     {
@@ -47,7 +64,7 @@ export class FinancialChartComponent implements OnInit {
     return Math.random() * (max - min) + min;
   }
 
-  randomBar(date: DateTime, lastClose: number): { c: number; t: DateTime; h: number; l: number; o: number } {
+  randomBar(date: Date, lastClose: number): { c: number; t: Date; h: number; l: number; o: number } {
     const open = this.randomNumber(lastClose * 0.95, lastClose * 1.05);
     const close = this.randomNumber(open * 0.95, open * 1.05);
     const high = this.randomNumber(Math.max(open, close), Math.max(open, close) * 1.1);
@@ -61,12 +78,12 @@ export class FinancialChartComponent implements OnInit {
     };
   }
 
-  getRandomData(dateStr: string, count: number): { c: number; t: DateTime; h: number; l: number; o: number }[] {
-    let date = DateTime.fromRFC2822(dateStr);
+  getRandomData(dateStr: string, count: number): { c: number; t: Date; h: number; l: number; o: number }[] {
+    let date = parseISO(dateStr);
     const data = [ this.randomBar(date, 30) ];
     while (data.length < count) {
-      date = date.plus({ days: 1 });
-      if (date.weekday <= 5) {
+      date = add(date, { days: 1 });
+      if (date.getDay()<= 5) {
         data.push(this.randomBar(date, data[data.length - 1].c));
       }
     }
