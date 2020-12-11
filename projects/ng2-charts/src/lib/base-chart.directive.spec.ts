694d4892e412a58de02d3a634911924425595655
Dan Manastireanu <498419+danmana@users.noreply.github.com>
2020-12-11 14:47:16 +0200

    fix tests

--- projects/ng2-charts/src/lib/base-chart.directive.spec.ts
+++ projects/ng2-charts/src/lib/base-chart.directive.spec.ts
@@ -1,10 +1,10 @@
 import { BaseChartDirective } from './base-chart.directive';
-import { ElementRef } from '@angular/core';
+import { ElementRef, NgZone } from '@angular/core';
 import { ThemeService } from './theme.service';
 
 describe('BaseChartDirective', () => {
   it('should create an instance', () => {
-    const directive = new BaseChartDirective(null as ElementRef, null as ThemeService);
+    const directive = new BaseChartDirective(null as ElementRef, null as ThemeService, null as NgZone);
     expect(directive).toBeTruthy();
   });
 });
