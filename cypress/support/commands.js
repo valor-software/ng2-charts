56efe123416ba0992d6248e7f67c6e4dd08fc325
Marco Santarelli <marco@backbase.com>
2021-04-29 17:08:20 +0200

    Show diff in console

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,5 +3,6 @@ import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 addMatchImageSnapshotCommand({
   failureThreshold: 0.01, // threshold for entire image
   failureThresholdType: 'percent', // percent of image or number of pixels
+  dumpInlineDiffToConsole: true,
   comparisonMethod: 'ssim'
 });
