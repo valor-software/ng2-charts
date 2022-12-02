f7059d718d9ad9aaec52c55ef3f6b58e14341bfe
Marco Santarelli <marco@backbase.com>
2022-12-02 23:20:47 +0100

    Fixed width charts

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,8 +5,8 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:4200/",
     chromeWebSecurity: false,
-    videosFolder: "../dist/cypress/ng2-charts-e2e/videos",
-    screenshotsFolder: "../dist/cypress/ng2-charts-e2e/screenshots",
+    videosFolder: "dist/cypress/videos",
+    screenshotsFolder: "dist/cypress/screenshots",
     setupNodeEvents(on, config) {
       // implement node event listeners here
       initPlugin(on, config);
