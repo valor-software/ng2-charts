bf18d25709b7d33b4fc7dae2cb56f67e6687c2bb
Marco Santarelli <santa.85@gmail.com>
2023-08-11 11:42:04 +0200

    Exclude demo app unit tests for the moment

--- apps/ng2-charts-demo/src/app/bubble-chart/bubble-chart.component.ts
+++ apps/ng2-charts-demo/src/app/bubble-chart/bubble-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 
 @Component({
