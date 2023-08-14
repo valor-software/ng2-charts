1fd30befc606d81e6e2ffc41cf025651f0bc2ffa
Marco Santarelli <santa.85@gmail.com>
2023-08-14 16:03:50 +0200

    Try forcing viewport on browser args

--- apps/ng2-charts-e2e/cypress.config.ts
+++ apps/ng2-charts-e2e/cypress.config.ts
@@ -17,24 +17,9 @@ export default defineConfig({
       initPlugin(on, config);
 
       on('before:browser:launch', (browser, launchOptions) => {
-        if (browser.name === 'chrome' && browser.isHeadless) {
-          // fullPage screenshot size is 1400x1200 on non-retina screens
-          // and 2800x2400 on retina screens
-          launchOptions.args.push('--window-size=1000,1000')
-
-          // force screen to be non-retina (1400x1200 size)
-          launchOptions.args.push('--force-device-scale-factor=1')
-        }
-
-        if (browser.name === 'electron' && browser.isHeadless) {
+        if (browser.name === 'electron') {
           // fullPage screenshot size is 1400x1200
-          launchOptions.preferences['width'] = 1000
-        }
-
-        if (browser.name === 'firefox' && browser.isHeadless) {
-          // menubars take up height on the screen
-          // so fullPage screenshot size is 1400x1126
-          launchOptions.args.push('--width=1000')
+          launchOptions.preferences['width'] = 800
         }
 
         return launchOptions
