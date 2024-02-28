1f8491c1d290af39f7a5795478515d9688b5aa3e
Adrien Crivelli <adrien.crivelli@gmail.com>
2024-02-28 18:21:16 +0800

    Migrate to new control flow syntax (#1827)
    Co-authored-by: Marco Santarelli <santam85@users.noreply.github.com>

--- apps/ng2-charts-demo/src/app/line-chart/line-chart.component.ts
+++ apps/ng2-charts-demo/src/app/line-chart/line-chart.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import { MatButton } from '@angular/material/button';
-import { NgFor } from '@angular/common';
+
 import { ChartHostComponent } from '../chart-host/chart-host.component';
 
 @Component({
@@ -10,7 +10,7 @@ import { ChartHostComponent } from '../chart-host/chart-host.component';
   templateUrl: './line-chart.component.html',
   styleUrls: ['./line-chart.component.scss'],
   standalone: true,
-  imports: [NgFor, MatButton, BaseChartDirective, ChartHostComponent],
+  imports: [MatButton, BaseChartDirective, ChartHostComponent],
 })
 export class LineChartComponent {
   private newLabel? = 'New label';
