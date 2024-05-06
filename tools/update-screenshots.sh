412695e0699b269e3e9ab2f7f19f22f7d99e779a
Marco Santarelli <santa.85@gmail.com>
2024-05-06 11:10:46 +0200

    Update screenshots

--- tools/update-screenshots.sh
+++ tools/update-screenshots.sh
@@ -1,5 +1,5 @@
-docker run -it -v "$PWD":/workdir -w /workdir node:lts npx playwright install-deps &&
+docker run -it -v "$PWD":/workdir -w /workdir node:lts "npx playwright install-deps &&
 npx playwright install &&
 npm run build:lib &&
 npm run build:prod &&
-npm run e2e:update-snapshots
+npm run e2e:update-snapshots"
