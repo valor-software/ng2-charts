ebfcf08c70de8e001b73c396545c9a00532d2aa0
Umanskiy Aleksey <sharkakatsukie@gmail.com>
2026-03-05 13:30:06 +0200

    chore(version): fixed CI test run

--- apps/ng2-charts-demo/jest.config.ts
+++ apps/ng2-charts-demo/jest.config.ts
@@ -1,4 +1,4 @@
-const esModules = ['lodash-es'].join('|');
+const esModules = ['lodash-es', 'marked', 'ngx-markdown'].join('|');
 export default {
   displayName: 'ng2-charts',
   preset: '../../jest.preset.js',
@@ -17,6 +17,24 @@ export default {
   transformIgnorePatterns: [`node_modules/(?!.*\\.mjs$|${esModules})`],
   moduleNameMapper: {
     '.*\\.txt': 'jest-raw-loader',
+    '^@angular/core/testing$':
+      '<rootDir>/../../node_modules/@angular/core/fesm2022/testing.mjs',
+    '^@angular/common/testing$':
+      '<rootDir>/../../node_modules/@angular/common/fesm2022/testing.mjs',
+    '^@angular/common/http$':
+      '<rootDir>/../../node_modules/@angular/common/fesm2022/http.mjs',
+    '^@angular/common/http/testing$':
+      '<rootDir>/../../node_modules/@angular/common/fesm2022/http-testing.mjs',
+    '^@angular/platform-browser/testing$':
+      '<rootDir>/../../node_modules/@angular/platform-browser/fesm2022/testing.mjs',
+    '^@angular/platform-browser/animations$':
+      '<rootDir>/../../node_modules/@angular/platform-browser/fesm2022/animations.mjs',
+    '^@angular/router$':
+      '<rootDir>/../../node_modules/@angular/router/fesm2022/router.mjs',
+    '^@angular/router/testing$':
+      '<rootDir>/../../node_modules/@angular/router/fesm2022/testing.mjs',
+    '^@angular/material/(.+)$':
+      '<rootDir>/../../node_modules/@angular/material/fesm2022/$1.mjs',
   },
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
